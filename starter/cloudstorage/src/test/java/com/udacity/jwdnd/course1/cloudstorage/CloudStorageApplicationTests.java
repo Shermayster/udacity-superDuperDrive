c6f63cc09e6619684f77dff5f360e9c3b2972969
pavelshermayster <shermayster@gmail.com>
2021-01-08 21:45:01 +0200

    init project

--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -2,10 +2,12 @@ package com.udacity.jwdnd.course1.cloudstorage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudStorageApplicationTests {
@@ -17,12 +19,13 @@ class CloudStorageApplicationTests {
 
 	@BeforeAll
 	static void beforeAll() {
-		WebDriverManager.chromedriver().setup();
+
+		WebDriverManager.firefoxdriver().setup();
 	}
 
 	@BeforeEach
 	public void beforeEach() {
-		this.driver = new ChromeDriver();
+		this.driver = new FirefoxDriver();
 	}
 
 	@AfterEach
@@ -36,6 +39,17 @@ class CloudStorageApplicationTests {
 	public void getLoginPage() {
 		driver.get("http://localhost:" + this.port + "/login");
 		Assertions.assertEquals("Login", driver.getTitle());
+	}
+
+	@Test
+	public void signupUser() {
+		String username = "superDuper";
+		String password = "admin";
+		driver.get("http://localhost:" + this.port + "/signup");
+		SignupPage signupPage = new SignupPage(driver);
+
+		//should not show success message
+		assertFalse(signupPage.isSuccessMessageInDocument());
 	}
 
 }
---
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/SignupPage.java
@@ -0,0 +1,55 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignupPage {
+    @FindBy(css = "#inputFirstName")
+    private WebElement firstNameField;
+
+    @FindBy(css = "#inputLastName")
+    private WebElement lastNameField;
+
+    @FindBy(css = "#inputUsername")
+    private WebElement usernameField;
+
+    @FindBy(css = "#inputPassword")
+    private WebElement passwordField;
+
+    @FindBy(css = "#submit-button")
+    private WebElement submitButton;
+
+    @FindBy(css = "[data-testid='success-message']")
+    private WebElement successMessage;
+
+    @FindBy(css = "[data-testid='login-link']")
+    private WebElement loginLink;
+
+    public SignupPage(WebDriver webDriver) {
+        PageFactory.initElements(webDriver, this);
+    }
+
+    public void signup(String firstName, String lastName, String username, String password) {
+        this.firstNameField.sendKeys(firstName);
+        this.lastNameField.sendKeys(lastName);
+        this.usernameField.sendKeys(username);
+        this.passwordField.sendKeys(password);
+        this.submitButton.click();
+    }
+
+    public boolean isSuccessMessageInDocument() {
+        try {
+            return successMessage.isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+
+    }
+
+    public void goToLogin() {
+        this.loginLink.click();
+    }
+}
